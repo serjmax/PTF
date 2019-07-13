56ee3ecf5c6efe2eb86c6ca9cf92c6f56d4e177c
Sergey Maximov <Serj.max@gmail.com>
2019-07-13 19:54:34 +0200

    add smart css grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './smart-grid.css'
 
 function App() {
+  
+  const numbers = new Array(50);
+  for (let i = 1; i < numbers.length; i++) {
+    numbers[i] = i;  
+  } 
+  const listItems = numbers.map((number) =>
+    <div key={number} style={{border: "solid 1px black"}} className="styling one">{number}</div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>{listItems}</ul>
+      <div className="main">
+      <div style={{border: "solid 1px black"}} className="styling one">1</div>
+      <div style={{border: "solid 1px black"}} className="styling one">2</div>
+      <div style={{border: "solid 1px black"}} className="styling one">4</div>
+      <div style={{border: "solid 1px black"}} className="styling one">3</div>
+      <div style={{border: "solid 1px black"}} className="styling one">5</div>
+      <div style={{border: "solid 1px black"}} className="styling one">6</div>
+      <div style={{border: "solid 1px black"}} className="styling one">7</div>
+      <div style={{border: "solid 1px black"}} className="styling one">8</div>
+      <div style={{border: "solid 1px black"}} className="styling one">9</div>
+      <div style={{border: "solid 1px black"}} className="styling one">10</div>
+      <div style={{border: "solid 1px black"}} className="styling one">11</div>
+      <div style={{border: "solid 1px black"}} className="styling one">12</div>
+      </div>
     </div>
   );
 }
