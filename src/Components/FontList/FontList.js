9bb562a39feea4d3afb37db258497e81ec2b5e0f
Sergey Maximov <Serj.max@gmail.com>
2019-07-14 21:27:08 +0200

    update font array

--- src/Components/FontList/FontList.js
+++ src/Components/FontList/FontList.js
@@ -4,8 +4,8 @@ import FontExample from '../FontExample/FontExample';
 export default class FontList extends Component {
   state = {
     fonts: [
-      {id: 1, fontSize: "144pt", lineHeight: "250px", marginTop: "-44px", link: 'AFForm-Regular', name: 'AFForm', word: 'Дух времени', language: "Latin / Кириллица", price: 2000},
-      {id: 2, fontSize: "78pt", lineHeight: "184px", marginTop: "31px", link: 'Form-Bold', name: 'Form Bold', word: 'Бесстрашие', language: "Latin / Кириллица", price: 2000},
+      {id: 1, fontSize: "144pt", lineHeight: "250px", marginTop: "-49px", link: 'AFForm-Regular', name: 'AFForm', word: 'Дух времени', language: "Latin / Кириллица", price: 2000},
+      {id: 2, fontSize: "78pt", lineHeight: "184px", marginTop: "28px", link: 'Form-Bold', name: 'Form Bold', word: 'Бесстрашие', language: "Latin / Кириллица", price: 2000},
       {id: 3, fontSize: "165px", lineHeight: "203px", marginTop: "-12px",link: 'Germanica-Regular', name: 'Germanica', word: 'дизайн', language: "Latin / Кириллица", price: 2000},
     ]
   }
