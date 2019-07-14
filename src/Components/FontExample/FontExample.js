3247de460d0d136853b83a9f8b5b2ba3ea80aef1
Sergey Maximov <Serj.max@gmail.com>
2019-07-14 21:26:15 +0200

    update font example comp

--- src/Components/FontExample/FontExample.js
+++ src/Components/FontExample/FontExample.js
@@ -1,42 +1,121 @@
 import React, { Component } from 'react'
+import './FontExample.scss'
 
 export default class FontExample extends Component {
+
+  state = {
+    isToggleOn: true,
+  };
     
-    render() {
-      console.log('props', this.props);
-      
-        return (      
-            <div style={{height: "252px"}} className="twelve nested">
-                <div className="four body_20x24">
-                  {this.props.name}
-                </div>
+  handleFocus = () => {
+    this.setState(state => ({
+      isToggleOn: false
+    }));
+  }
 
-                <div className="four body_20x24">
-                  {this.props.language}
-                </div>
+  handleUnhide = () => {
+    this.setState(state => ({
+      isToggleOn: true
+    }));
+  }
 
-                <div style={{textAlign: "end"}} className="four body_20x24">
-                  {this.props.price} ₽
-                </div>
-                <div style={{
-                    borderTop: "solid 2px", 
-                    marginLeft: "-14px",
-                    marginRight: "-14px",
-                    marginTop: "-50px"
+  render() {
+    console.log('props', this.props);
+    
+      return (      
+          <div 
+            style={{
+              height: "252px",
+              width: "100%"
+            }} 
+            className="">
+
+              <div 
+                className="body_20x24"
+                style={{
+                  width: "336px",
+                  display: "inline-block"
+                }}  
+              >
+                {this.props.name}
+              </div>
+
+              <div 
+                className="body_20x24"
+                style={{
+                  width: "336px",
+                  display: "inline-block"
+                }}
+                >{this.props.language}</div>
+
+              <div 
+                className="body_20x24"
+                style={{
+                  width: "324px",
+                  display: "inline-block",
+                  textAlign: "end"
                 }} 
-                className="twelve"></div>
-                <div 
-                  style={{
-                    fontFamily: `${this.props.link}`,
-                    fontSize: "118pt", 
-                    // height: "278px", 
-                    marginTop: "-146px",
-                    lineHeight: "160pt"}}
-                    className="twelve"
-                    >
+              >{this.props.price} ₽</div>
+
+              <div 
+                style={{
+                  borderTop: "solid 2px", 
+                  marginLeft: "-14px",
+                  paddingRight: "28px",
+                  marginTop: "2px",
+                  width: "100%"
+                }}>  
+              </div>
+
+              <input
+                onClick={this.handleFocus}
+                style={{
+                  zIndex: `${this.state.isToggleOn ? "0" : "2"}`,
+                  fontFamily: `${this.props.link}`,
+                  fontSize: `${this.props.fontSize}`, 
+                  lineHeight: `${this.props.lineHeight}`,
+                  width: "100%",
+                  border: "none",
+                  padding: "0",
+                  backgroundColor: "#ffffff00",
+                  marginTop: `${this.props.marginTop}`,
+                  position: "relative",
+                }} 
+                type="text" 
+                placeholder={this.props.word}
+                name="name"
+                className="input-font" 
+              />
+
+              <div
+                onClick={this.handleUnhide} 
+                style={{
+                  width: "100%",
+                  backgroundColor: `${this.state.isToggleOn ? "white" : "white"}`,
+                  position: `${this.state.isToggleOn ? "relative" : "fixed"}`,
+                  height: `${this.state.isToggleOn ? "100%" : "100vh"}`,
+                  zIndex: `${this.state.isToggleOn ? "0" : "1"}`,
+                  top: `${this.state.isToggleOn ? "" : "0"}`,
+                  left: `${this.state.isToggleOn ? "" : "0"}`,
+                  display: `${this.state.isToggleOn ? "none" : "block"}`
+                }}
+              
+              >
+              </div>
+              {/* <div
+                
+                style={{
+                  fontFamily: `${this.props.link}`,
+                  fontSize: "118pt", 
+                  // height: "278px", 
+                  marginTop: "-146px",
+                  lineHeight: "160pt"}}
+                  className="twelve"
+                  >
                     {this.props.word}
-                </div>
-            </div>
-        )
-    }
+                  
+              </div> */}
+          </div>
+      )
+  }
 }
--- src/Components/FontList/FontList.js
+++ src/Components/FontList/FontList.js
@@ -4,9 +4,9 @@ import FontExample from '../FontExample/FontExample';
 export default class FontList extends Component {
   state = {
     fonts: [
-      {id: 1, link: 'AFForm-Regular', name: 'AFForm', word: 'Дух времени', language: "Latin / Кириллица", price: 2000},
-      {id: 2, link: 'Form-Bold', name: 'Form Bold', word: 'Бесстрашие', language: "Latin / Кириллица", price: 2000},
-      {id: 3, link: 'Germanica-Regular', name: 'Germanica', word: 'дизайн', language: "Latin / Кириллица", price: 2000},
+      {id: 1, fontSize: "144pt", lineHeight: "250px", marginTop: "-44px", link: 'AFForm-Regular', name: 'AFForm', word: 'Дух времени', language: "Latin / Кириллица", price: 2000},
+      {id: 2, fontSize: "78pt", lineHeight: "184px", marginTop: "31px", link: 'Form-Bold', name: 'Form Bold', word: 'Бесстрашие', language: "Latin / Кириллица", price: 2000},
+      {id: 3, fontSize: "165px", lineHeight: "203px", marginTop: "-12px",link: 'Germanica-Regular', name: 'Germanica', word: 'дизайн', language: "Latin / Кириллица", price: 2000},
     ]
   }
   render() {
@@ -21,6 +21,9 @@ export default class FontList extends Component {
               word={font.word}
               language={font.language}
               price={font.price}
+              fontSize={font.fontSize}
+              lineHeight={font.lineHeight}
+              marginTop={font.marginTop}
               selected={true}/>
           )} 
       </div>
