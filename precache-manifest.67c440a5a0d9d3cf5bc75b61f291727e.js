dfea39ebb4ddeed6f5858e1e81698802a06e24b7
Sergey Maximov <Serj.max@gmail.com>
2019-07-14 22:24:25 +0200

    Updates

---
+++ precache-manifest.67c440a5a0d9d3cf5bc75b61f291727e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108b9eadfad87302d6c386ebe4270a9e",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "01b3c6411791702849ea",
+    "url": "/react-gh-pages/static/css/main.b5562ef6.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/react-gh-pages/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "01b3c6411791702849ea",
+    "url": "/react-gh-pages/static/js/main.aa5a1c03.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "b3cff1c7853de4c04485767cd67d678e",
+    "url": "/react-gh-pages/static/media/AFForm-Regular.b3cff1c7.woff"
+  },
+  {
+    "revision": "dc5513ee0ae44212f942dab956b0e5b9",
+    "url": "/react-gh-pages/static/media/Form-Bold.dc5513ee.woff"
+  },
+  {
+    "revision": "c393f2c639e8f72be5e7ffe56e5d2394",
+    "url": "/react-gh-pages/static/media/Germanica-Regular.c393f2c6.woff"
+  },
+  {
+    "revision": "934400eed7779aff0779e5cea44e6e5c",
+    "url": "/react-gh-pages/static/media/KW kiosk sign.934400ee.svg"
+  },
+  {
+    "revision": "c90d2f5f8b96b6560560ee2c42b8ec30",
+    "url": "/react-gh-pages/static/media/Repro-Regular.c90d2f5f.woff"
+  },
+  {
+    "revision": "5f3c699d15260403c7ce2518c8d6bea4",
+    "url": "/react-gh-pages/static/media/kiosk_logo_color.5f3c699d.svg"
+  }
+]);
