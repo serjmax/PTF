8bb354e805af44f6eac319678899c5424ce221bc
Sergey Maximov <Serj.max@gmail.com>
2019-09-08 20:36:46 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import './App.scss';
 
+import Main from './pages/Main/Main'
+import Manifesto from './pages/Manifesto/Manifesto'
 
 function App() {
 
   return (
+        <div className="App">
+          <Router>
+            <Route path="/" exact component={Main} />
+            <Route path="/manifesto/" component={Manifesto} />   
+         </Router> 
     </div>
   );
 }
