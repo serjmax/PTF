39eec1917f5a855dae3e3e264e6af91861432d86
Sergey Maximov <Serj.max@gmail.com>
2019-09-08 18:40:44 +0200

    update comp cross selling

--- src/Components/CrossSelling/CrossSelling.js
+++ src/Components/CrossSelling/CrossSelling.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react'
 import './CrossSelling.scss'
 
+import {ReactComponent as Manifesto5} from '../../Images/manifesto_5.svg'
+
 export default class FontExample extends Component {
 
   render() {
       return (      
-        <div 
-          style={{
-            backgroundColor: 'white'
-          }}
-          className="cross-selling">
-            <div 
-            className="cross-selling__header"
-            style={{}}
-            >
-            THE REAL PLAY TYPEFOUNDRY
+        <div className="cross-selling">
+              <div className="cross-selling__container">
+                <div className="cross-selling__header" style={{ gridColumn: "1 / 13", gridRow: "1 / 1"}}>
+                  THE REAL PLAY TYPEFOUNDRY
+                </div>
+                <div className="cross-selling__image" style={{ gridColumn: "1 / 7", gridRow: "2 / 8"}}>
+                <Manifesto5 style={{width: '274px'}}/>
+                </div>
+                <div className="cross-selling__content" style={{ gridColumn: "7 / 12", gridRow: "2 / 7"}} >
+                  <p>мы принимаем в библиотеку шрифты, созданные с помощью модульных линеек designworkout® + playtype,  другие экспериментальные шрифты.</p>
+                  <a className="cross-selling__content-link" style={{marginTop: '12px', display: 'inline-block'}} href="mailto:vladimir.kolomeytcev@gmail.com?subject=Вова, хочу продавать свой шрифт">пиши вове коломейцеву</a>
+                  <p style={{marginTop: '49px', display: 'inline-block'}}>если хочешь научиться создавать шрифты, приходи в сampus на недельные интенсивы</p>
+                  <a className="cross-selling__content-link" style={{marginTop: '12px', display: 'inline-block'}} href="https://campus.designworkout.ru/playtype/">designworkout playtype</a>
+                </div>
+                <p style={{ gridColumn: "2 / 7", gridRow: "7 / 7", alignSelf: 'end', fontSize: '18px'}}>оперативная связь</p>
+                <p style={{ gridColumn: "7 / 12", gridRow: "7 / 7", alignSelf: 'end', fontSize: '18px'}}>группа <a className="cross-selling__content-link" href="https://t.me/designworkout_org">designworkout org</a> в telegram</p>
             </div>
-            <div className="cross-selling__image"></div>
-            <div className="cross-selling__content"></div>
-            <div className="cross-selling__footer"></div>
         </div>
       )
   }
