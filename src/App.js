9377a9b9ec8d3d80d9221312303beb199cd94467
Sergey Maximov <Serj.max@gmail.com>
2019-09-11 11:44:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import './App.scss';
 
 import Main from './pages/Main/Main'
-import Manifesto from './pages/Manifesto/Manifesto'
 
 function App() {
 
   return (
-        <div className="App">
-          <Router>
-            <Route path="/" exact component={Main} />
-            <Route path="/manifesto/" component={Manifesto} />   
-         </Router> 
+    <div className="App">
+      <Main/>
     </div>
   );
 }
