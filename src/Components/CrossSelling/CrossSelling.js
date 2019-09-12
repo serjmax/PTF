cb84dcde29c21d9fd0fd88740554d851e2880d13
Sergey Maximov <Serj.max@gmail.com>
2019-09-12 10:20:30 +0200

    Update CrossSelling.js

--- src/Components/CrossSelling/CrossSelling.js
+++ src/Components/CrossSelling/CrossSelling.js
@@ -10,13 +10,13 @@ export default class FontExample extends Component {
         <div className="cross-selling">
               <div className="cross-selling__container">
                 <div className="cross-selling__header" style={{ gridColumn: "1 / 13", gridRow: "1 / 1"}}>
-                  THE REAL PLAY TYPEFOUNDRY
+                  PLAY FACES TYPEFOUNDRY
                 </div>
                 <div className="cross-selling__image" style={{ gridColumn: "1 / 7", gridRow: "2 / 8"}}>
                 <Manifesto5 style={{width: '274px'}}/>
                 </div>
                 <div className="cross-selling__content" style={{ gridColumn: "7 / 12", gridRow: "2 / 7"}} >
-                  <p>мы принимаем в библиотеку шрифты, созданные с помощью модульных линеек designworkout® + playtype,  другие экспериментальные шрифты.</p>
+                  <p>мы принимаем в библиотеку шрифты, созданные с помощью модульных линеек designworkout® + playtype и другие экспериментальные шрифты.</p>
                   <a className="cross-selling__content-link" style={{marginTop: '12px', display: 'inline-block'}} href="mailto:vladimir.kolomeytcev@gmail.com?subject=Вова, хочу продавать свой шрифт">пиши вове коломейцеву</a>
                   <p style={{marginTop: '49px', display: 'inline-block'}}>если хочешь научиться создавать шрифты, приходи в сampus на недельные интенсивы</p>
                   <a className="cross-selling__content-link" style={{marginTop: '12px', display: 'inline-block'}} href="https://campus.designworkout.ru/playtype/">designworkout playtype</a>
